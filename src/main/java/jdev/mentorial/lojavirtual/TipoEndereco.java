e76321da08d3dc3d7d0b6476da7b094c46ad2e42
Athos Gustavo <athosgustavowf@gmail.com>
2024-07-14 17:27:28 -0300

    Mapeando as classes

---
+++ src/main/java/jdev/mentorial/lojavirtual/TipoEndereco.java
@@ -0,0 +1,21 @@
+package jdev.mentorial.lojavirtual;
+
+public enum TipoEndereco {
+	
+	COBRANCA("Cobrança"),
+	ENTREGA("Entrega");
+	
+	private String descricao;
+
+	TipoEndereco(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	// Instância: TipoEndereco.COBRANCA, retorna o nome da instância "COBRANCA"
+	// Instância: TipoEndereco.COBRANCA.getDescricao() retorna o valor "Cobrança"
+
+}
--- src/main/java/jdev/mentorial/lojavirtual/model/Endereco.java
+++ src/main/java/jdev/mentorial/lojavirtual/model/Endereco.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,11 +13,14 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
+import jdev.mentorial.lojavirtual.TipoEndereco;
 
 @Entity
 @SequenceGenerator(name = "seq_endereco", sequenceName = "seq_endereco", allocationSize = 1, initialValue = 1)
 public class Endereco implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_endereco")
 	private Long id;
@@ -32,16 +37,97 @@ public class Endereco implements Serializable {
     private Pessoa pessoa;
 	
 	//Ler-se: Vários endereços podem estar associado a apenas uma pessoa.
-	//A anotação @JoinColumn é usada para especificar detalhes sobre a coluna que será usada como chave estrangeira em uma relação de entidade.
+	///A anotação @JoinColumn é usada para especificar detalhes sobre a coluna que será usada como chave estrangeira em uma relação de entidade.
 	//name define o nome da chave estrangeira
 	//Dica:O mapeamento é realizado no lado muitos.
 	
-	//private ENUM tipoEndereco;
-	
-	
+	@Enumerated(EnumType.STRING)
+	private TipoEndereco tipoEndereco;
 	
 	public Endereco() {
 		// TODO Auto-generated constructor stub
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRuaLogra() {
+		return ruaLogra;
+	}
+
+	public void setRuaLogra(String ruaLogra) {
+		this.ruaLogra = ruaLogra;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public TipoEndereco getTipoEndereco() {
+		return tipoEndereco;
+	}
+
+	public void setTipoEndereco(TipoEndereco tipoEndereco) {
+		this.tipoEndereco = tipoEndereco;
+	}
+	
+	
 
 }
